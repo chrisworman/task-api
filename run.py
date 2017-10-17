d2552b88f3d684f28a38b10f48cbfb603e3ee955
Chris Worman <chrisworman@gmail.com>
2017-10-17 12:41:11 -0600

    Added threaded=True to app.run call

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import create_app
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app(config_name)
-app.run(host= "0.0.0.0")
+app.run(host= "0.0.0.0", threaded=True)
