eb1a2dcfd6561920837696343d486bf9a0b8c3ef
Chris Worman <chrisworman@gmail.com>
2017-10-20 12:37:28 -0600

    Dealt with db changes in dev Docker container

--- create-dev-user.sql
+++ create-dev-user.sql
@@ -1,2 +1,2 @@
 alter user postgres password 'dev-password';
-create user task_api_user createdb password 'dev-password';
+create user root createdb password 'dev-password';
--- dev.sh
+++ dev.sh
@@ -4,15 +4,20 @@ if [ $1 = "docker" ]; then
   if [ $2 = "build" ]; then
     docker build -f Dockerfile.dev -t task-api-dev .
   elif [ $2 = "run" ]; then
-    docker run -v `pwd`:/usr/src/app:ro -i -t -p 5000:5000 task-api-dev
+    docker run -v `pwd`:/usr/src/app -i -t -p 5000:5000 task-api-dev
   fi
 elif [ $1 = "db" ]; then
   if [ $2 = "start" ]; then
    service postgresql start
   elif [ $2 = "init" ]; then
+    service postgresql start
     su -c 'createdb tasks_api' - postgres
     su -c 'psql -U postgres -d tasks_api -a -f /usr/src/app/create-dev-user.sql' - postgres
-    python manage.py db upgrade
+    python /usr/src/app/manage.py db upgrade
+  elif [ $2 = "migrate" ]; then
+    python /usr/src/app/manage.py db migrate
+  elif [ $2 = "upgrade" ]; then
+    python /usr/src/app/manage.py db upgrade
   elif [ $2 = "stop" ]; then
     service postgresql stop
   fi
@@ -21,5 +26,11 @@ elif [ $1 = "server" ]; then
       flask run &
     elif [ $2 = "stop" ]; then
       pkill flask
+    fi
+elif [ $1 = "requirements" ]; then
+    if [ $2 = "freeze" ]; then
+      pip freeze > requirements.txt
+    elif [ $2 = "install" ]; then
+      pip install --no-cache-dir -r requirements.txt
     fi
 fi
