a037592e3f0a29807ac7f85349717cd6d766b428
Chris Worman <chrisworman@gmail.com>
2017-10-20 19:08:15 -0600

    Better error handling with usage info

--- dev.sh
+++ dev.sh
@@ -12,6 +12,9 @@ if [ $1 = "docker" ]; then
     docker build -f Dockerfile.dev -t task-api-dev .
   elif [ $2 = "run" ]; then
     docker run -v `pwd`:/usr/src/app -i -t -p 5000:5000 task-api-dev
+  else
+    echo "Command not recognized: $2.  Try build or run"
+    exit 1
   fi
 elif [ $1 = "db" ]; then
   if [ $2 = "start" ]; then
@@ -27,17 +30,31 @@ elif [ $1 = "db" ]; then
     python /usr/src/app/manage.py db upgrade
   elif [ $2 = "stop" ]; then
     service postgresql stop
+  else
+    echo "Command not recognized: $2.  Try start, init, migrate, upgrade, or stop"
+    exit 1
   fi
 elif [ $1 = "server" ]; then
     if [ $2 = "start" ]; then
       flask run &
     elif [ $2 = "stop" ]; then
       pkill flask
+    else
+      echo "Command not recognized: $2.  Try start or stop"
+      exit 1
     fi
 elif [ $1 = "requirements" ]; then
     if [ $2 = "freeze" ]; then
       pip freeze > requirements.txt
     elif [ $2 = "install" ]; then
       pip install --no-cache-dir -r requirements.txt
+    else
+      echo "Command not recognized: $2.  Try freeze or install"
+      exit 1
     fi
+ else
+   echo "System not recognized: $1.  Try docker, db, server, or requirements"
+   exit 1
 fi
+
+exit 0
