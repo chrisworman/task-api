0b26321e372e47812db3f10f8330a3f1e837bbe5
Chris Worman <chrisworman@gmail.com>
2017-10-25 15:55:43 -0600

    Switched from Flask-Api app to vanilla Flask app (artifact from tutorial)

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 #!env/bin/python
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-from flask import request, jsonify, abort
+from flask import request, jsonify, abort, Flask
 from flask_cors import CORS
 from instance.config import app_config
 import pprint
@@ -14,7 +14,7 @@ def create_app(config_name):
     from app.models import Task
     from app.models import List
 
-    app = FlaskAPI(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
