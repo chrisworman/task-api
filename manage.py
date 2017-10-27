52f176574028b80c3ef1132dccfd25db2c3bbd8e
Chris Worman <chrisworman@gmail.com>
2017-10-26 19:28:32 -0600

    Simplified app config

--- manage.py
+++ manage.py
@@ -1,9 +1,8 @@
 import os
-from flask_script import Manager # class for handling a set of commands
+from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from main import db, app
 
-#app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
 manager = Manager(app)
 
